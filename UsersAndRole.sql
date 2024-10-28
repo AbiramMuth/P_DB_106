38b16d231bfdca14df14cefb9af792848ed06188
AbiramMuth <ps33gsc@eduvaud.ch>
2024-10-28 14:41:23 +0100

    Last week: doc rendus

--- UsersAndRole.sql
+++ UsersAndRole.sql
@@ -1,21 +1,23 @@
+-- Attention supprimer d'abord les Utilisateurs et les rôles
 -- Administrateur du jeu
 CREATE ROLE Administrateur;
 
 -- Création de l’utilisateur :
-CREATE USER 'lise'@'localhost' IDENTIFIED BY 'ldje-aw';
+CREATE USER `lise`@`` IDENTIFIED BY 'lise';
+
+
 
 -- Privilèges de l’utilisateur :
 GRANT ALL PRIVILEGES ON db_space_invaders.* to Administrateur with grant option;
 
 -- lise dans le rôle d'admin
-GRANT 'Administrateur' TO 'lise'@'localhost';
-
+GRANT 'Administrateur' TO 'lise'@'';
 
 -- Joueur
 CREATE ROLE Joueur;
 
 -- Création de l’utilisateur :
-CREATE USER 'bob'@'localhost' IDENTIFIED BY 'bjsb_de';
+CREATE USER 'bob'@'' IDENTIFIED BY 'bob';
 
 -- Privilèges de l’utilisateur :
 -- -- Droit de lecture des informations sur la table t_arme.
@@ -28,7 +30,7 @@ GRANT CREATE ON db_space_invaders.t_commande to Joueur;
 GRANT SELECT ON db_space_invaders.t_commande to Joueur;
 
 -- bob dans le rôle joueur
-GRANT 'Joueur' TO 'bob'@'localhost';
+GRANT 'Joueur' TO 'bob'@'';
 
 
 -- Gestionnaire de la boutique
@@ -36,18 +38,24 @@ GRANT 'Joueur' TO 'bob'@'localhost';
 CREATE ROLE GestionnaireBoutique;
 
 -- Création de l’utilisateur :
-CREATE USER 'carl'@'localhost' IDENTIFIED BY 'cakf';
+CREATE USER 'carl'@'' IDENTIFIED BY 'carl';
 
 
 -- Privilèges de l’utilisateur :
 -- Droit de lecture dans la table t_joueur.
-GRANT SELECT ON db_space_invaders.t_joueur to 'GestionnaireBoutique';
+GRANT SELECT ON db_space_invaders.t_joueur to GestionnaireBoutique;
 
 -- Droit de mise à jour, lecture, et suppression des armes.
-GRANT UPDATE, SELECT, DELETE ON db_space_invaders.t_arme to 'GestionnaireBoutique’;
+GRANT UPDATE, SELECT, DELETE ON db_space_invaders.t_arme to GestionnaireBoutique;
 
 -- Droit de lecture dans la table t_commande.
-GRANT SELECT ON db_space_invaders.t_commande to 'GestionnaireBoutique’;
+GRANT SELECT ON db_space_invaders.t_commande to GestionnaireBoutique;
 
 -- carl dans le rôle Gestionnaire
-GRANT 'GestionnaireBoutique' TO 'carl'@'localhost';
+GRANT 'GestionnaireBoutique' TO 'carl'@'';
+
+SET DEFAULT ROLE 'Administrateur' TO 'lise'@'';
+SET DEFAULT ROLE 'Joueur' TO 'bob'@'';
+SET DEFAULT ROLE 'GestionnaireBoutique' TO 'carl'@'';
+
+
